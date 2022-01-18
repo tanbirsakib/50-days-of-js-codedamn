d261de8b84ce7e13eca478307d662234428cd845
Tanbir Sakib <tanbirsakib>
2022-01-18 19:56:44 +0600

    problem 8

---
+++ day-08/differrenceBetweenDate.js
@@ -0,0 +1,14 @@
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
+function getDaysBetweenDates(dateText1, dateText2) {
+    const date1 = new Date(dateText1);
+    const date2 = new Date(dateText2);
+
+    const diff = date2.getTime() - date1.getTime()
+    const days = diff/DAY_IN_MILLISECONDS;
+ 
+
+    return days
+}
+
+console.log(`Days difference: ${getDaysBetweenDates('10/15/2020', '12/1/2020')}`)
